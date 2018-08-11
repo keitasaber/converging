c0f05a748026f295352fd2ce84722a010156ed3b
Your Name <keitasaber@gmail.com>
2018-08-11 13:40:52 +0700

    add notification service

--- Converging/app/components/product_categories/productCategoryListController.js
+++ Converging/app/components/product_categories/productCategoryListController.js
@@ -1,9 +1,9 @@
 ﻿(function (app) {
     app.controller('productCategoryListController', productCategoryListController);
 
-    productCategoryListController.$inject = ['$scope', 'apiService'];
+    productCategoryListController.$inject = ['$scope', 'apiService', 'notificationService'];
 
-    function productCategoryListController($scope, apiService) {
+    function productCategoryListController($scope, apiService, notificationService) {
         $scope.productCategories = [];
         $scope.page = 0;
         $scope.pagesCount = 0;
@@ -26,6 +26,10 @@
             }
 
             apiService.get('/api/productcategory/get', config, function (result) {
+                if (result.data.TotalCount == 0) {
+                    notificationService.displayWarning("Không có danh mục nào được tìm thấy");
+                }
+
                 $scope.productCategories = result.data.Items;
                 $scope.page = result.data.Page;
                 $scope.pagesCount = result.data.TotalPages;
---
+++ Converging/app/shared/serivces/notificationService.js
@@ -0,0 +1,52 @@
+﻿(function (app) {
+    app.factory('notificationService', notificationService);
+
+    function notificationService() {
+        toastr.options = {
+            "closeButton": false,
+            "debug": false,
+            "newestOnTop": true,
+            "progressBar": true,
+            "positionClass": "toast-top-right",
+            "preventDuplicates": false,
+            "onclick": null,
+            "showDuration": "300",
+            "hideDuration": "1000",
+            "timeOut": "5000",
+            "extendedTimeOut": "1000",
+            "showEasing": "swing",
+            "hideEasing": "linear",
+            "showMethod": "fadeIn",
+            "hideMethod": "fadeOut"
+        }
+        return {
+            displaySuccess: displaySuccess,
+            displayError: displayError,
+            displayWarning: displayWarning,
+            displayInfo: displayInfo
+        }
+
+        function displaySuccess(message) {
+            toastr.success(message)
+        }
+
+        function displayError(error) {
+            if (Array.isArray(error)) {
+                error.each(function (error) {
+                    toastr.error(error)
+                })
+            }
+            else {
+                toastr.displayError(error)
+            }
+        } 
+
+        function displayWarning(message) {
+            toastr.warning(message)
+        }
+
+        function displayInfo(message) {
+            toastr.info(message)
+        }
+    }
+})(angular.module('converging.common'))
